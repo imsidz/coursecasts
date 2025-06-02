10fbdee89b71456ef6188461857827db4ce5243c
Marian Pirvan <pirvan.marian@gmail.com>
2025-06-02 20:32:02 +0300

    category 16,sample ,seedr ,abstract

---
+++ database/seeders/KubernetesResourceSchedulingSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+class KubernetesResourceSchedulingSeeder extends PerformanceDiscussionSeeder
+{
+    protected int $discussionId = 553;
+    protected string $title = 'Kubernetes Resource Scheduling for High-Performance Data Pipelines';
+    protected int $topicId = 16;
+    protected array $tagNames = ['kubernetes', 'resource scheduling', 'data pipelines'];
+    protected string $postFile = 'kubernetes_resource_scheduling_posts.php';
+}
